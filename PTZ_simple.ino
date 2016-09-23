3d473678f7755b8c677d788a2326d6a60c042df5
cbradburne <cbradburne@gmail.com>
2016-09-23 01:05:17 +0100

    Update PTZ_simple.ino

--- PTZ_simple.ino
+++ PTZ_simple.ino
@@ -68,9 +68,9 @@ const int constrainedLimit = 120;         // how many steps does the acceleratio
 const int startingYSpeed = 30;            // Y accel start speed
 const int startingXSpeed = 30;            // X accel start speed
 
-//const int myArray[30]={0,1,1,2,3,4,5,6,7,8,10,11,12,14,15,16,18,19,20,22,23,24,25,26,27,28,29,29,30};
-//const int myArray[30]={0,0,0,1,1,1,2,3,3,4,5,6,7,8,9,10,11,12,14,15,16,18,19,21,22,24,25,27,28,30};
-//const int myArray[30]={0,0,0,0,1,1,1,1,2,2,2,3,3,4,4,5,6,6,7,8,8,9,10,10,11,12,13,13,14,15};
+//const int myArray[30]={0,1,1,2,3,4,5,6,7,8,10,11,12,14,15,16,18,19,20,22,23,24,25,26,27,28,29,29,30};    // half sinewave (30 p-p)
+//const int myArray[30]={0,0,0,1,1,1,2,3,3,4,5,6,7,8,9,10,11,12,14,15,16,18,19,21,22,24,25,27,28,30};      // quarter sinewave (60 p-p)
+//const int myArray[30]={0,0,0,0,1,1,1,1,2,2,2,3,3,4,4,5,6,6,7,8,8,9,10,10,11,12,13,13,14,15};             // quarter sinewave (30 p-p)
 int logAccel;
 
 int Yread;
